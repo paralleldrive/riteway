a3e5b72d867855473fe07ffa6c19b4087b0852f9
Eric Elliott <support@paralleldrive.com>
2017-08-14 13:51:26 -1000

    Initial commit

---
+++ source/riteway.js
@@ -0,0 +1,30 @@
+import tape from 'tape';
+
+// The testing library: a thin wrapper around tape
+const describe = (unit, cb) => tape(unit, assert => {
+  const end = assert.end.bind(assert);
+
+  const result = cb(description => ({
+    assert: ({ actual, expected, given, should = description }) =>
+      assert.same(
+        actual, expected,
+        `Given ${ given }: should ${ should }`
+      ),
+    // We can probably use async/await to deal with most async tests,
+    // but we should probably still supply `end()` so users can do
+    // whatever they like
+    end
+  }));
+
+  if (result && result.then) result.then(end);
+});
+
+const Try = (fn, ...args) => {
+  try {
+    return fn(...args);
+  } catch (err) {
+    return err;
+  }
+};
+
+export { describe, Try };
