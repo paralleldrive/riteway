b59a93e930e8c4539fc05f5adf5ad32f0dd9d737
Eric Elliott <support@paralleldrive.com>
2018-09-04 17:23:40 -0700

    Update index.d.ts
    Fix `Try` array type.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare module 'riteway' {
   export function describe(label: string, callback: describeCallback): void
   
-  export function Try(fn: Function, ...args: Array<number>): any
+  export function Try(fn: Function, ...args: Array<any>): any
 
   type describeCallback = (should: should) => Promise<void>
 
