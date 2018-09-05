fac64558bb41292e914a6d5b5595d98252df0148
Eric Elliott <support@paralleldrive.com>
2018-09-04 17:17:08 -0700

    Update index.d.ts
    Untested. Please review.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
 declare module 'riteway' {
   export function describe(label: string, callback: describeCallback): void
+  
+  export function Try(fn: Function, ...args: Array<number>): any
 
   type describeCallback = (should: should) => Promise<void>
 
