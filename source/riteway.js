6512815bc3b20f3a3dd384518c8e0322a52a9187
Geireann Lindfield Roberts <geireann.lindfield@gmail.com>
2024-01-11 15:46:12 -0800

    revert

--- source/riteway.js
+++ source/riteway.js
@@ -8,13 +8,7 @@ const concatToString = (keys, key, index) => keys + (index ? ', ' : '') + key;
 const withRiteway = TestFunction => test => {
   const end = () => test.end();
 
-  const assert = ({
-    // initialize values to undefined so TypeScript doesn't complain
-    given = undefined,
-    should = undefined,
-    actual = undefined,
-    expected = undefined
-  }) => {
+  const assert = (args = {}) => {
     const missing = requiredKeys.filter((k) => !Object.keys({ given, should, actual, expected }).includes(k));
   if (missing.length) {
     throw new Error(
@@ -24,7 +18,15 @@ const withRiteway = TestFunction => test => {
       )}`
     );
   }
-  
+
+    const {
+      // initialize values to undefined so TypeScript doesn't complain
+      given = undefined,
+      should = undefined,
+      actual = undefined,
+      expected = undefined
+    } = args;
+
     test.same(
       actual, expected,
       `Given ${given}: should ${should}`
