73c2583e919b0ddc9ac9164f88299a206beb8537
Geireann Lindfield Roberts <geireann.lindfield@gmail.com>
2024-01-19 11:02:35 -0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,44 +1,40 @@
-/// <reference types='cheerio' />
-/// <reference types='react' />
+/// <reference types="cheerio" />
+/// <reference types="react" />
 
 declare module 'riteway' {
-  export function Try<U extends any[], V>(
-    fn: (...args: U) => V,
-    ...args: U
-  ): any | Promise<any>;
 
-  export function createStream(opts: CreateStreamOptions): ReadableStream;
+  export function Try<U extends any[], V>(fn: (...args: U) => V, ...args: U): any | Promise<any>
+
+  export function createStream(opts: CreateStreamOptions): ReadableStream
 
   export const describe: DescribeFunction;
 
   interface DescribeFunction {
-    (unit: string, testFunction: TestFunction): Promise<void>;
-    only: (unit: string, testFunction: TestFunction) => Promise<void>;
-    skip: (unit: string, testFunction: TestFunction) => Promise<void>;
+    (unit: string, testFunction: TestFunction): Promise<void>
+    only: (unit: string, testFunction: TestFunction) => Promise<void>
+    skip: (unit: string, testFunction: TestFunction) => Promise<void>
   }
 
-  type assert = <T>(assertion: Assertion<T>) => void;
+  type assert = <T>(assertion: Assertion<T>) => void
 
-  type TestFunction = (assert: assert, end?: Function) => Promise<void>;
+  type TestFunction = (assert: assert, end?: Function) => Promise<void>
 
   interface Assertion<T> {
-    readonly given: any;
-    readonly should: string;
-    readonly actual: T;
-    readonly expected: T;
+    readonly given: any
+    readonly should: string
+    readonly actual: T
+    readonly expected: T
   }
 
   interface CreateStreamOptions {
-    readonly objectMode: boolean;
+    readonly objectMode: boolean
   }
 }
 
-declare module 'riteway/render-component' {
+declare module "riteway/render-component" {
   export default function render(el: JSX.Element): cheerio.Root;
 }
 
-declare module 'riteway/match' {
-  export default function match(
-    text: string
-  ): (pattern: string | RegExp) => string;
+declare module "riteway/match" {
+  export default function match(text: string): (pattern: string | RegExp) => string;
 }
